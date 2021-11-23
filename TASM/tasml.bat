89b5467b2a443921beb639470058bb8a411782cf
5trange <neon.lights.cx@protonmail.com>
2021-11-23 18:04:45 +0530

    Source + TASM bin

---
+++ TASM/tasml.bat
@@ -0,0 +1,20 @@
+@echo off
+cd\
+cd tasm
+tasm
+cls
+Echo.
+@echo off
+Echo  +================================================================+
+Echo    	Pain heals, Chicks dig scars, Glory lasts forever!
+Echo  +================================================================+
+Echo ___________________________________________________________________
+Echo  Type proper command to perform the desired action
+Echo ___________________________________________________________________
+Echo  Command	    Action
+ECHO  edit		-   Open MS-DOS Editor
+ECHO  tasm 		-   Compilation
+Echo  tlink		-   Perform Linking
+Echo  td		-   Launch Turbo Debugger
+Echo  exit		-   Exit TASM
+Echo ___________________________________________________________________
---
+++ launch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd DOSBOXConf
+dosbox -conf "tasm_mount.conf"
